cbb26578f9dc6e053963c2a8773d0bbca9811a3d
Bernardo Caiado <becaiado@gmail.com>
2020-11-30 12:36:31 -0800

    model is fully functional

--- app.py
+++ app.py
@@ -5,63 +5,11 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
-image_size = st.slider('Zoom', 50, 250, 119)
-
-
-TEACHERS = {
-    'Bruno Lajoie' : {"url":'https://avatars1.githubusercontent.com/u/22095643?v=4', "rating": 0},
-    'Kevin Robert' : {"url":'https://avatars1.githubusercontent.com/u/9978111?v=4', "rating": 0},
-    'Filme3' : {"url":'https://avatars1.githubusercontent.com/u/22095643?v=4', "rating": 0},
-    'Filme4' : {"url":'https://avatars1.githubusercontent.com/u/9978111?v=4', "rating": 0},
-    'Filme5' : {"url":'https://avatars1.githubusercontent.com/u/22095643?v=4', "rating": 0},
-    'Filme6' : {"url":'https://avatars1.githubusercontent.com/u/9978111?v=4', "rating": 0},
-}
-TEACHER_CSS = f"""
-#teachers {{
-    display: flex;
-    flex-wrap: wrap;
-}}
-.teacher-card {{
-    display: flex;
-    flex-direction: column;
-}}
-.teacher-card img {{
-    width: {image_size}px;
-    height: {image_size}px;
-    border-radius: 100%;
-    padding: 4px;
-    margin: 10px;
-    box-shadow: 0 0 4px #eee;
-}}
-.teacher-card span {{
-    text-align: center;
-}}
-"""
-# streamlit html injection seems to sensitive to new lines...
-# remove that \ and the html gets displayed instead of being interpreted
-TEACHER_CARD = """\
-    <div class="teacher-card">
-        <img src="{url}">
-        <span>{name}</span>
-        <span>{rating}</span>
-    </div>
-"""
-teachers = ''.join([TEACHER_CARD.format(name=f'{name.split()[0]}', url=info["url"], rating=st.sidebar.number_input(name,1,5)) for name, info in TEACHERS.items()])
-TEACHER_HTML = f"""
-<style>
-{TEACHER_CSS}
-</style>
-<div id="teachers">
-    {teachers}
-</div>
-"""
-st.write(TEACHER_HTML, unsafe_allow_html=True)
-
 st.title('Rogue Inteligent Movie Recomendation systems')
 
 dt = pd.read_csv('path.csv')
-df = pd.read_csv('df.csv')
-random_subset = dt.head(3)
+df = pd.read_csv('out.csv')
+random_subset = dt.head(5)
 
 key = 0
 ratings = []
